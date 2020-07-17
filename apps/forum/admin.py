121907f4a7fb91932604f8184632f29d796d5020
jeremiando <situmeang92.fs@gmail.com>
2020-07-17 22:53:05 +0700

    Add Model Forum in admin and Add Comment in Forum for admin

--- apps/forum/admin.py
+++ apps/forum/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Forum,Comment
 
 # Register your models here.
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+class ForumAdmin(admin.ModelAdmin):
+    inlines = [CommentInline,]
+    
+admin.site.register(Forum,ForumAdmin)
